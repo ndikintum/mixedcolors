f0c97549f954233b1babe25cd12a0e2d515b2270
ndikintum <ndikintumrodrique2@gmail.com>
2023-12-05 07:22:51 +0100

    correcting linters

--- assets/main.js
+++ assets/main.js
@@ -1,29 +1,29 @@
-function isPrime(num) {
-  if (num <= 1) return false;
+function isPrime (num) {
+  if (num <= 1) return false
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) return false
   }
   return true
 }
 
-function generateNumbers() {
-  const container = document.getElementById("numberContainer");
+function generateNumbers () {
+  const container = document.getElementById('number-container')
 
   for (let i = 0; i < 10000; i++) {
-    const number = Math.floor(Math.random() * 1000) + 1
-    const cell = document.createElement("div")
+    const number = Math.floor(Math.random () * 1000) + 1
+    const cell = document.createElement('div')
 
     // Set the text content of the cell to the generated number
     cell.textContent = number
 
     if (number % 2 === 0) {
-      cell.classList.add("cell", "even")
+      cell.classList.add('cell', 'even')
     } else {
-      cell.classList.add("cell", "odd")
+      cell.classList.add('cell', 'odd')
     }
 
     if (isPrime(number)) {
-      cell.classList.add("prime")
+      cell.classList.add('prime')
     }
 
     container.appendChild(cell)
